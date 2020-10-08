3634b985d2680e064e3c05b6c1c3a936d5e95928
TmsTheo <72571490+TmsTheo@users.noreply.github.com>
2020-10-08 20:25:48 +0200

    Add files via upload

---
+++ Projet_Robot/IO.h
@@ -0,0 +1,12 @@
+#ifndef IO_H
+#define IO_H
+
+//Affectation des pins des LEDS
+#define LED_ORANGE _LATC10 
+#define LED_BLEUE _LATG7
+#define LED_BLANCHE _LATG6
+
+// Prototypes fonctions
+void InitIO();
+
+#endif /* IO_H */
---
+++ Projet_Robot/timer.h
@@ -0,0 +1,8 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+void InitTimer23(void);
+void InitTimer1(void);
+
+#endif /* TIMER_H */
+
